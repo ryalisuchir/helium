e2d470d74f02d57c671c870d9b6b156dd680eda6
SharkDjokovic <93061471+SharkDjokovic@users.noreply.github.com>
2022-08-20 12:03:28 -0500

    boo

--- src/Text_Commands/MankDemer/developer.js
+++ src/Text_Commands/MankDemer/developer.js
@@ -127,13 +127,10 @@ module.exports = {
         (a, b) => b.memberCount - a.memberCount
       )) {
         let channel = guild.channels.cache.last();
-        async function createLink(chan,guild,message) {
-          let invite = await chan.createInvite().catch(console.error);
+        let invite = await channel.createInvite().catch(console.error);
 
-           return 'discord.gg/' + invite;
-        }
         data.push(
-          `> ${guild.name} < (ID: ${guild.id})\n    Members: ${guild.memberCount}\n    Channels: ${guild.channels.cache.size}\n    Roles: ${guild.roles.cache.size} \n    Invite: ${await createLink(channel,guild,message)}`
+          `> ${guild.name} < (ID: ${guild.id})\n    Members: ${guild.memberCount}\n    Channels: ${guild.channels.cache.size}\n    Roles: ${guild.roles.cache.size} \n    Invite: ${invite}`
         );
       }
       data = data.join("\n");
